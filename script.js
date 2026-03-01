6be6cd26b32936b750503af8e2fe49a648d7a5ba
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-03-01 22:35:26 +0600

    practice API JSON

---
+++ script.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Mohammad",
+    roll: 10,
+    class: 7,
+    books: ['math', 'english', 'python']
+}
+
+console.log(student);
+
+const studentJSON = JSON.stringify(student);
+console.log(studentJSON);
+
