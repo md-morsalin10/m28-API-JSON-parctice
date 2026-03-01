4ff656020f40c0a896501daefe602619d7b6418c
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-03-02 00:45:32 +0600

    use fetch method

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-const student = {
-    name: "Mohammad",
-    roll: 10,
-    class: 7,
-    books: ['math', 'english', 'python']
+// const student = {
+//     name: "Mohammad",
+//     roll: 10,
+//     class: 7,
+//     books: ['math', 'english', 'python']
+// }
+
+// console.log(student);
+
+// const studentJSON = JSON.stringify(student);
+// console.log(studentJSON);
+
+// const studentPhrase =JSON.parse(studentJSON);
+// console.log(studentPhrase, typeof studentPhrase);
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response=> response.json()
+//        .then(data=> console.log(data)));
+
+
+
+const jsonBtn = () => {
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json()
+            .then(data => console.log(data)));
 }
 
-console.log(student);
+const loadComment = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(url)
+    .then(res => res.json()
+    .then(data => {
+        console.log(data);
+        displayPost(data);
+    }))
+}
 
-const studentJSON = JSON.stringify(student);
-console.log(studentJSON);
-
+const displayPost = (posts) => {
+    posts.forEach((post=>{
+        console.log(post)
+    }))
+}
